f9a3d97164b390cec428e823d147e1f574ddbeb7
Plat <github@p1at.dev>
2022-08-22 16:21:35 +0900

    chore: update deps versions

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
-export * from "https://deno.land/x/manga_solver@v1.0.4/mod.ts";
-export * from "https://deno.land/x/oak@v10.4.0/mod.ts";
-export * from "https://deno.land/x/jszip@0.11.0/mod.ts";
-export * from "https://deno.land/std@0.130.0/testing/asserts.ts";
+export * from "https://deno.land/x/manga_solver@v1.0.4/mod.ts"
+export * from "https://deno.land/x/oak@v11.0.0/mod.ts"
+export * from "https://deno.land/x/jszip@0.11.0/mod.ts"
+export * from "https://deno.land/std@0.152.0/testing/asserts.ts"
 // export * as log from "https://deno.land/std@0.130.0/log/mod.ts";
-export * from "https://deno.land/x/rss@0.5.5/mod.ts";
-export * as Colors from "https://deno.land/std@0.130.0/fmt/colors.ts";
+export * from "https://deno.land/x/rss@0.5.6/mod.ts"
+export * as Colors from "https://deno.land/std@0.152.0/fmt/colors.ts"
